f8bf6a9ba62d2280bd104c26c0465b94ea994789
Vladislav Surin <vlad_surin@mail.ru>
2024-03-23 20:28:01 +0300

    App routing has been replaced with the React Router library

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,17 +1,17 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 import { USERS } from '@/data';
-import { Transit, usePages } from '@/PagesProvider';
 import './UsersPage.css';
 
 export function UsersPage() {
-  const { setSearchParam, search } = usePages();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const handleSearchName = (event: ChangeEvent<HTMLInputElement>): void => {
     const { value } = event.target;
-    setSearchParam('searchName', value.toLowerCase());
+    setSearchParams({ searchName: value.toLowerCase() });
   };
 
-  const searchName = new URLSearchParams(search).get('searchName') || '';
+  const searchName = searchParams.get('searchName') || '';
 
   const filteredUsers = USERS.filter(({ fullName }) =>
     fullName.toLowerCase().includes(searchName),
@@ -32,12 +32,12 @@ export function UsersPage() {
         </label>
 
         {filteredUsers.map(({ id, fullName }) => (
-          <Transit
+          <Link
             to={`/users/${id}`}
             key={id}
           >
             {fullName}
-          </Transit>
+          </Link>
         ))}
       </div>
     </div>
